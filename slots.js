23a4eedaa22cade3cf3d95bf4b64324bb60b8db7
Jokes <et.irrumabo@gmail.com>
2019-06-16 09:23:46 -0400

    rolling works now

--- slots.js
+++ slots.js
@@ -12,8 +12,8 @@ function addSymbol(sym, id) {
         pay = 2;
     }
     var symhtml = '<li id="slot-' + id + '" class="slot"> <ul class="slot"> \
-    <li>Symbol: <input type="text" class="ipt_symbol" name="slot-' + id + '_symbol" value="' + sym + '"></li> \
-    <li>Payout: <input type="text" class="ipt_payout" name="slot-' + id + '_payout" value="' + pay + '"></li> </ul></li>';
+    <li>Symbol: <input type="text" class="ipt_symbol" id="slot-' + id + '_symbol" value="' + sym + '"></li> \
+    <li>Payout: <input type="text" class="ipt_payout" id="slot-' + id + '_payout" value="' + pay + '"></li> </ul></li>';
     $("#slots-list").append(symhtml);
 }
 
@@ -21,14 +21,16 @@ function randomSlot() {
     return Math.floor(Math.random() * $("#slotcount").text() + 1);
 }
 
+var sym = function(v) {
+    return {
+        symbol: $("#slot-" + v + "_symbol").val(),
+        payout: $("#slot-" + v + "_payout").val()
+    }
+}
+
 function fullRoll() {
     var rolln = [randomSlot(), randomSlot(), randomSlot()];
-    var roll = rolln.map(function(v) {
-        return {
-            symbol: $("#slot-" + v + "_symbol").val(),
-            payout: $("#slot-" + v + "_payout").val()
-        }
-    });
+    var rolls = rolln.map(sym);
 
     var event = 1;
     var payout = 0;
@@ -36,17 +38,33 @@ function fullRoll() {
     var b = rolln[1];
     var c = rolln[2];
     if (a == b) {
-        payout = roll[0].payout;
+        payout = rolls[0].payout;
+        event = 2;
         if (b == c) {
             event = 3;
         }
-        event = 2;
     } else if (b == c || a == c) {
-        payout = roll[2].payout;
+        payout = rolls[2].payout;
         event = 2;
     }
 
-    return { roll: roll, event: event, payout: payout }
+    return { roll: rolls, event: event, payout: payout }
+}
+
+function rollHtml(r) {
+    console.log(r);
+    console.log(r.roll);
+    var rh = r.roll.map(function(v) {
+        return '<li class="rollsym">' + v.symbol + '</li>'
+    }).join("");
+    var payclass = "payout-none";
+    if (r.payout > 0) { payclass = "payout-yes" }
+    return '<li class="rollcard event-' + r.event + ' ' + payclass + '"><ul>' + rh + '</ul></li>'
+}
+
+function cutRollSection() {
+    $("#rolls-current").removeAttr("id");
+    $("#roll-history").append('<ul id="rolls-current" class="roll-list"></ul>');
 }
 
 $(document).ready(function() {
@@ -67,6 +85,31 @@ $(document).ready(function() {
 
     $("#btn_roll").click(function() {
         var thisRoll = fullRoll();
+        $("#rolls-current").append(rollHtml(thisRoll));
+    });
+
+    $("#btn_roll_e").click(function() {
+        var event = 1;
+        var thisRoll;
+        cutRollSection();
+
+        while (event < 2) {
+            thisRoll = fullRoll();
+            $("#rolls-current").append(rollHtml(thisRoll));
+            event = thisRoll.event;
+        }
+    });
+
+    $("#btn_roll_p").click(function() {
+        var payout = 0;
+        var thisRoll;
+        cutRollSection();
+
+        while (payout == 0) {
+            thisRoll = fullRoll();
+            $("#rolls-current").append(rollHtml(thisRoll));
+            payout = thisRoll.payout;
+        }
     });
 
 });
