9320382891f28b1c23b2b84f00e05acc4158e71d
Jokes <et.irrumabo@gmail.com>
2019-06-26 20:03:37 -0400

    roll style works now; stats don't

--- slots.js
+++ slots.js
@@ -29,40 +29,53 @@ function getNumSlots() {
     return $("#ipt_slotcount").val();
 }
 
-function fullRoll() {
+function fullRoll(styleIsAdd) {
     var slotn = getNumSlots();
     var rolln = [];
     for (var i = 0; i < slotn; i++) {
-        console.log(rolln.push([randomSlot()]));
+        rolln.push([randomSlot()]);
     }
     var rolls = rolln.map(symInfo);
 
     var event = 1;
     var payout = 0;
-    var a = rolln[0];
-    var b = rolln[1];
-    var c = rolln[2];
-    if (a == b) {
-        payout = rolls[0].payout;
-        event = 2;
-        if (b == c) {
-            event = 3;
+
+    if (styleIsAdd) {
+        event = slotn;
+    }
+
+    var rollcounts = {};
+    for (var i = 0; i < rolls.length; i++) {
+        var s = rolls[i].symbol;
+        if (rollcounts.hasOwnProperty(s)) {
+            rollcounts[s]++;
+            event = Math.max(event, rollcounts[s]);
+        } else {
+            rollcounts[s] = 1;
         }
-    } else if (b == c || a == c) {
-        payout = rolls[2].payout;
-        event = 2;
+    }
+    for (var i = 0; i < rolls.length; i++) {
+        rolls[i].count = rollcounts[rolls[i].symbol];
+        if (styleIsAdd || rolls[i].count > 1) {
+            payout += rolls[i].payout * rolls[i].count; // approximation, not final
+        }
     }
 
     return { roll: rolls, event: event, payout: payout }
 }
 
-function rollHtml(r) {
+function rollHtml(r, styleIsAdd) {
     var rh = r.roll.map(function(v) {
-        return '<li class="rollsym">' + v.symbol + '</li>'
+        var liclass = "rollsym";
+        if (v.payout > 0) { liclass += " sym-payout"; }
+        if (v.count > 1) { liclass += " sym-combo sym-combo-" + v.count; }
+        return '<li class="' + liclass + '">' + v.symbol + '</li>'
     }).join("");
     var payclass = "payout-none";
+    var evclass = "event-" + r.event;
     if (r.payout > 0) { payclass = "payout-yes" }
-    return '<li class="rollcard event-' + r.event + ' ' + payclass + '"><ul>' + rh + '</ul></li>'
+    if (styleIsAdd) { evclass = "event-add" }
+    return '<li class="rollcard ' + evclass + ' ' + payclass + '"><ul>' + rh + '</ul></li>'
 }
 
 function cutRollSection() {
@@ -71,17 +84,27 @@ function cutRollSection() {
 }
 
 function updateStats(count, pcount, ptotal) {
-    // unfortunately, combinatorics
-    var perm = count * count * count;
+    var slotn = getNumSlots();
+    var styleIsCombo = $("#sct_style").children("option:selected").val() == "combo";
+    var payChance;
+    var ev;
 
-    var notOne = (count - 1) * (count - 1) * count; // the thing, but two of them aren't this symbol
-    var yesOne = perm - notOne;
-    var yesAny = yesOne * pcount; // that times the number of things
-    var payChance = yesAny / perm;
-    var smallPayChance = (yesAny - pcount) / perm; // big payoff happens with three of a kind
-    var bigPayChance = pcount / perm; // like so
-    var avgPay = ptotal / pcount;
-    var ev = avgPay * smallPayChance + avgPay * 10 * bigPayChance - $("#ipt_price").val();
+    if (styleIsCombo) {
+        // unfortunately, combinatorics
+        var perm = Math.pow(count, slotn);
+
+        var notOne = (count - 1) * (count - 1) * count; // the thing, but two of them aren't this symbol
+        var yesOne = perm - notOne;
+        var yesAny = yesOne * pcount; // that times the number of things
+        payChance = yesAny / perm;
+        var smallPayChance = (yesAny - pcount) / perm; // big payoff happens with three of a kind
+        var bigPayChance = pcount / perm; // like so
+        var avgPay = ptotal / pcount;
+        ev = avgPay * smallPayChance + avgPay * 10 * bigPayChance - $("#ipt_price").val();
+    } else {
+        // for now, assume additive
+
+    }
 
     $("#disp_ev").text("" + ev);
     $("#disp_odds").text("" + payChance);
@@ -109,32 +132,39 @@ $(document).ready(function() {
     });
 
     $("#btn_roll").click(function() {
-        var thisRoll = fullRoll();
-        $("#rolls-current").append(rollHtml(thisRoll));
+        var styleIsAdd = $("#sct_style").children("option:selected").val() == "additive";
+        var thisRoll = fullRoll(styleIsAdd);
+        $("#rolls-current").append(rollHtml(thisRoll, styleIsAdd));
     });
 
     $("#btn_roll_e").click(function() {
+        var styleIsAdd = $("#sct_style").children("option:selected").val() == "additive";
         var event = 1;
         var thisRoll;
         cutRollSection();
 
-        while (event < 2) {
-            thisRoll = fullRoll();
-            $("#rolls-current").append(rollHtml(thisRoll));
+        for (var attempts = 0; event < 2 && attempts < 1000; attempts++) {
+            thisRoll = fullRoll(styleIsAdd);
+            $("#rolls-current").append(rollHtml(thisRoll, styleIsAdd));
             event = thisRoll.event;
         }
     });
 
     $("#btn_roll_p").click(function() {
+        var styleIsAdd = $("#sct_style").children("option:selected").val() == "additive";
         var payout = 0;
         var thisRoll;
         cutRollSection();
 
-        while (payout == 0) {
-            thisRoll = fullRoll();
-            $("#rolls-current").append(rollHtml(thisRoll));
+        for (var attempts = 0; payout == 0 && attempts < 1000; attempts++) {
+            thisRoll = fullRoll(styleIsAdd);
+            $("#rolls-current").append(rollHtml(thisRoll, styleIsAdd));
             payout = thisRoll.payout;
         }
+    });
+
+    $("#btn_clear").click(function() {
+        $("#roll-history").html('<ul id="rolls-current" class="roll-list"></ul>');
     });
 
 });
